6510fc48330a5f068a1c03800c212d6c091b7023
Eswar B <eshwarboya340@gmail.com>
2026-08-18 21:28:20 +0530

    update script

--- first_script.sh
+++ first_script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # writing my first shell script
 echo "hello world , welcome to devops learning"
+name=eshwar
+if [ $name = "eshwar" ]; then
+  echo "given name is valid"
+else
+  echo "given name is invalid"
+fi
