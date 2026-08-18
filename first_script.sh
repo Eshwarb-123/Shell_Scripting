cc86eb8f51c4c0f66669f2a1fee6c7d717a3a626
root <root@ip-172-31-16-16.ec2.internal>
2026-08-18 16:47:18 +0000

    update

--- first_script.sh
+++ first_script.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-set -x
-# writing my first shell script
-echo "hello world , welcome to devops learning"
 if [ $1 = "eshwar" ]; then
  echo "Hi $1 welcome to the devops course"
 fi
 echo "=========== start addition operation ========="
 echo "enter first number"
 read a
-enter "enter second number"
+echo "enter second number"
 read b
 sum=$((a + b))
 echo "sum of the two numbers is : $sum"
