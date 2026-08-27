4e440da00852523317669ea279e0426aa12f1792
root <root@ip-172-31-16-16.ec2.internal>
2026-08-27 16:36:23 +0000

    read statement

--- read.sh
+++ read.sh
@@ -2,5 +2,14 @@
 
 read -p "Enter your name: " name
 echo "Hello $name"
+
+#read input without displaying it
+
 read -s -p "Enter your password: " password
 echo "$name , your password is $password"
+
+# read multiple values
+
+read -p "Enter you name and age: " name age
+echo "name is: $name "
+echo "age is: $age"
