c41f576c624a7c89055fffcf2deb0da7a136b885
Eswar Boya <eswarboya340@gmail.com>
2026-09-12 11:58:57 +0000

    trap

--- 10-loop.sh
+++ 10-loop.sh
@@ -2,6 +2,11 @@
 
 set -x
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 USERID=$(id -u)
 LOG_FOLDER=/var/log/shell-script/
 LOG_FILE=/var/log/shell-script/$0.log
@@ -10,7 +15,7 @@ mkdir -p $LOG_FOLDER
 
 if [ $USERID -ne 0 ]
 then
-  echo "please run script with root user access" | tee -a $LOG_FILE
+  echo " $R please run script with root user access $N" | tee -a $LOG_FILE
   exit 1
 fi
 
@@ -18,16 +23,16 @@ fi
 validate(){
    if [ $1 -ne 0 ]
    then 
-      echo "$2 ...Failure" | tee -a $LOG_FILE
+      echo "$R $2 ...Failure $N" | tee -a $LOG_FILE
       exit 1
    else
-      echo "$2 ...Successfull" | tee -a $LOG_FILE
+      echo "$R $2 ...Successfull $N" | tee -a $LOG_FILE
    fi
 }
 
 
 for package in $@  # sh validate.sh httpd nmap podman
 do
-  dnf install "$package" -y | tee -a "$LOG_FILE"
+  dnf remove "$package" -y | tee -a "$LOG_FILE"
   validate $? "$package installation"
 done
